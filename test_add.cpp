c1cf6994f63ac8b4f2fdc4a4f8b1a0a8eb64804d
Mary <maria.balakaeva@mail.ru>
2024-09-21 19:42:29 +0300

    auto unit tests in workflows

---
+++ test_add.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "func.hpp"
+
+
+TEST(Add, Simple){
+    EXPECT_EQ(my_add(2,2), 4);
+    EXPECT_EQ(my_add(2,3), my_add(3,2));
+    EXPECT_EQ(my_add(-42,3), -39);
+   
+    //EXPECT_NEAR(0.1+0.1+0.1,1e-7);
+
+}
