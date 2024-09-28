bb7ce9e74044533f3cd186647d6864774fd14204
Mary <maria.balakaeva@mail.ru>
2024-09-29 00:42:10 +0300

    configure documentation

--- src/func.cpp
+++ src/func.cpp
@@ -1,1 +1,8 @@
+/**
+ * @brief This program adds two numbers
+ * @param param1 double number
+ * @param param2 double number
+ * @return sum of two numbers.
+ */
+
 int my_add(int a, int b) { return a + b; }
